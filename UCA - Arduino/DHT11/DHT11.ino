de1c0ba192b3b8ec3e91d579eba3d854b57d8270
devon_p <89315745+parkkh07@users.noreply.github.com>
2022-07-23 18:26:06 +0900

    DHT 11

--- UCA - Arduino/DHT11/DHT11.ino
+++ UCA - Arduino/DHT11/DHT11.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include<DHT.h>
 
+#define DHTPIN A1
+#define DHTTYPE dht11
+
+DHT dht(DHTPIN,DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int h = dht.readHumidity();
+  int t = dht.readTemperature();
 
+  Serial.print("Humidity : ");
+  Serial.println(h);
+  Serial.print("Temperature : ");
+  Serial.println(t);s
 }
