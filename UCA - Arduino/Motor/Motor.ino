f26ceb839577216cf26963535c343571164abed7
devon_p <89315745+parkkh07@users.noreply.github.com>
2022-07-30 17:58:37 +0900

    Update Motor.ino

--- UCA - Arduino/Motor/Motor.ino
+++ UCA - Arduino/Motor/Motor.ino
@@ -4,17 +4,22 @@ Servo servo;
 
 void setup() {
   servo.attach(9);
+  Serial.begin(9600);
 }
 
+
 void loop() {
+  int readValue = analogRead(A0);
+  Serial.println(readValue);
+
+  //servo.write(readValue/6);
   
-  for(int i = 0;i<180;i++){
-    servo.write(i);
+  if(readValue >= 170){
+    servo.write(180);
     delay(10);
+    
+  }else{
+  servo.write(0);
   }
-  for(int j = 180;j>0;j--){
-    servo.write(j);
-    delay(10);
-  }
-  
+  delay(1000);
 }
